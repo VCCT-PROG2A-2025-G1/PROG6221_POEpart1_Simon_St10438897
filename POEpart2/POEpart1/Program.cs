1d98f0a771bb1f409ece1e23283ec16e14d8505f
ST10438897 <164011261+st10438897-SimonNoahSlingerland@users.noreply.github.com>
2025-05-24 13:56:21 +0200

    Start of POE part 2
    Implemented multiple responses for each keyword found

---
+++ POEpart2/POEpart1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+
+
+namespace POEpart1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CyberChat.PlaySound("IntroSound.wav");
+            CyberChat.Greeting();
+            CyberChat.Cyperchat();
+        }
+    }
+}
